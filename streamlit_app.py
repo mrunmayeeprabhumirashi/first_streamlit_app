c6ca7d0996605bd06186b3ae01351b5d252c14bf
mrunmayeeprabhumirashi <136655659+mrunmayeeprabhumirashi@users.noreply.github.com>
2023-06-15 00:49:39 +0100

    Update streamlit_app.py
    personalised and updated menu text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,10 @@
 import streamlit
 
-streamlit.title('My Parents New Healthy Diner')
+streamlit.title('My Mom's New Healthy Diner')
 
-streamlit.header('Breakfast Menu')
+streamlit.header('Breakfast Favourites')
 
-streamlit.text('Omega 3 and Bleuberry oatmeal')
-streamlit.text('Kale, Spinach and rocket smoothie')
-streamlit.text('Hard boiled free range eggs')
+streamlit.text('1. Omega 3 & Bleuberry Oatmeal')
+streamlit.text('2. Kale, Spinach and Rocket Smoothie')
+streamlit.text('3. Hard-Boiled Free Range Eggs')
+streamlit.text('4. Avocado Toast')
